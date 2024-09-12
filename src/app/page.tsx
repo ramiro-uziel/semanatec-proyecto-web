34fb141afa5ab98cb2304a1d877fb54922c28d40
ramiro-uziel <ramirouziel15@gmail.com>
2024-09-11 21:04:58 -0600

    Cambios UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,12 +13,13 @@ type AnimeResult = {
     };
   };
   title: string;
+  title_english: string;
   title_japanese: string;
   year: number;
   synopsis: string;
   episodes: number;
-  status: string;
   source: string;
+  airing: boolean;
 };
 
 export default function Home() {
@@ -51,7 +52,7 @@ export default function Home() {
       if (data.data && data.data.length > 0) {
         setAnimeResult(data.data[0]);
       } else {
-        setError("No anime found");
+        setError("No se encontró ningún anime con ese nombre.");
       }
     } catch (err) {
       setError("Failed to fetch anime. Please try again.");
@@ -92,6 +93,7 @@ export default function Home() {
             </button>
           </div>
         </form>
+        {error && <p className="text-red-500 text-center mt-5">{error}</p>}
       </motion.div>
 
       <AnimatePresence>
@@ -103,14 +105,17 @@ export default function Home() {
             transition={{ delay: 0.2 }}
             className="w-full max-w-2xl"
           >
-            {error && <p className="text-red-500 text-center mb-4">{error}</p>}
-
             {animeResult && (
               <div className="bg-white p-6 rounded-xl border border-black border-dashed">
-                <h2 className="text-2xl font-bold mb-2">{animeResult.title}</h2>
-                <p className="text-sm text-gray-600 mb-4">
-                  {animeResult.title_japanese}
-                </p>
+                <h2 className="text-2xl font-bold">
+                  {animeResult.title_english}
+                </h2>
+                <div className="flex flex-row space-x-3 items-center mb-4">
+                  <h3 className="text-lg font-medium">{animeResult.title}</h3>
+                  <p className="text-sm text-gray-600">
+                    {animeResult.title_japanese}
+                  </p>
+                </div>
                 <div className="flex mb-4">
                   <div className="w-1/3 mr-4">
                     <Image
@@ -123,15 +128,16 @@ export default function Home() {
                   </div>
                   <div className="w-2/3">
                     <p className="mb-2">
-                      <strong>Year:</strong> {animeResult.year || "N/A"}
+                      <strong>Año:</strong> {animeResult.year || "N/A"}
                     </p>
                     <p className="mb-2">
-                      <strong>Episodes:</strong> {animeResult.episodes}
+                      <strong>Episodios:</strong> {animeResult.episodes}
                     </p>
                     <p className="mb-2">
-                      <strong>Status:</strong> {animeResult.status}
+                      <strong>Status:</strong>{" "}
+                      {animeResult.airing ? "En emisión" : "Terminado"}
                     </p>
-                    <div className="h-32 overflow-y-auto mb-4 pr-2">
+                    <div className="h-40 overflow-y-auto mb-4 pr-2">
                       <p>{animeResult.synopsis}</p>
                     </div>
                     <a
@@ -140,15 +146,16 @@ export default function Home() {
                       rel="noopener noreferrer"
                       className="inline-block bg-blue-500 text-white px-4 py-2 rounded-xl hover:bg-blue-600 transition duration-300 ease-in-out"
                     >
-                      View on MyAnimeList
+                      Ver en MyAnimeList
                     </a>
                   </div>
                 </div>
-                {animeResult.source !== "Manga" && (
-                  <p className="text-yellow-600 bg-yellow-100 p-3 rounded-lg mt-4">
-                    Este anime no está basado en un manga.
-                  </p>
-                )}
+                {animeResult.source !== "Manga" &&
+                  animeResult.source !== "Web manga" && (
+                    <p className="text-yellow-600 bg-yellow-100 p-3 rounded-lg mt-4">
+                      Este anime no está basado en un manga.
+                    </p>
+                  )}{" "}
               </div>
             )}
           </motion.div>
